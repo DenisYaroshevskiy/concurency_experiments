90afb7825523dd8743ef45e211d9759ec6465d22
dyaroshev <denis.yaroshevskij@gmail.com>
2026-03-09 12:36:12 +0000

    mailbox

---
+++ include/mailbox.h
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2026 Denis Yaroshevskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <atomic_wrappers.h>
+
+#include <array>
+#include <vector>
+
+namespace tools {
+/*
+ * Single producer single consumer.
+ */
+template <typename T>
+class mailbox {
+ public:
+  mailbox() = default;
+  mailbox(const mailbox&) = delete;
+  mailbox(mailbox&&) = delete;
+  mailbox& operator=(const mailbox&) = delete;
+  mailbox& operator=(mailbox&&) = delete;
+
+  // always succeedes
+  void push(T x);
+
+  // returns all avaliable mail (or nothing if there is a concurrent push)
+  bool try_get(std::vector<T>& out);
+
+ private:
+  std::array<std::vector<T>, 2> vecs_;
+  tools::atomic<std::vector<T>*> senders_{&vecs_[0]};
+};
+
+template <typename T>
+void mailbox<T>::push(T x) {
+  auto* locked = senders_.exchange(nullptr, tools::memory_order_acquire);
+  locked->push_back(std::move(x));
+
+  /*
+   * maybe free some memory here in the future in case
+   * the vector has grown too big.
+   */
+
+  senders_.exchange(locked, tools::memory_order_release);
+}
+
+template <typename T>
+bool mailbox<T>::try_get(std::vector<T>& out) {
+  auto* cur = senders_.load(tools::memory_order_relaxed);
+  // locked
+  if (!cur) {
+    return false;
+  }
+
+  // Trying to switch the publisher to a different buffer.
+  auto* desired = cur == &vecs_[0] ? &vecs_[1] : &vecs_[0];
+  if (!senders_.compare_exchange_strong(
+          cur, desired,
+          tools::memory_order_acq_rel,
+          tools::memory_order_relaxed)) {
+    return false;
+  }
+
+  // We don't want to just move the vector to avoid moving the memory
+  // in malloc.
+  out.assign(std::make_move_iterator(cur->begin()),
+             std::make_move_iterator(cur->end()));
+  cur->clear();
+  return true;
+}
+
+}  // namespace tools
---
+++ rl_tests/mailbox_rl_test.cpp
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2026 Denis Yaroshevskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "relacy/context.hpp"
+#include "relacy/thread_local.hpp"
+#define RL_TEST
+#include "mailbox.h"
+
+#include <relacy/relacy.hpp>
+#include <relacy/test_suite.hpp>
+#include <relacy/var.hpp>
+
+#include <numeric>
+#include <vector>
+
+struct move_only {
+  int val_ = -1;
+  bool moved_from_ = false;
+
+  explicit move_only(int v) : val_(v) {}
+  move_only(const move_only&) = delete;
+  move_only& operator=(const move_only&) = delete;
+  move_only(move_only&& o) noexcept : val_(o.val_) { o.moved_from_ = true; }
+  move_only& operator=(move_only&& o) noexcept {
+    val_ = o.val_;
+    o.moved_from_ = true;
+    return *this;
+  }
+};
+
+struct mailbox_simple : rl::test_suite<mailbox_simple, 2> {
+  tools::mailbox<int> mb;
+
+  void thread(unsigned idx) {
+    if (idx == 0) {
+      mb.push(42);
+    } else {
+      std::vector<int> out;
+      while (!mb.try_get(out) || out.empty()) {
+        rl::yield(1, $);
+      }
+      RL_ASSERT(out.size() == 1);
+      RL_ASSERT(out[0] == 42);
+    }
+  }
+};
+
+struct mailbox_memory_order : rl::test_suite<mailbox_memory_order, 2> {
+  tools::mailbox<int> mb;
+  rl::var<int> shared_data{0};
+
+  void thread(unsigned idx) {
+    if (idx == 0) {
+      shared_data($) = 42;
+      mb.push(1);
+    } else {
+      std::vector<int> out;
+      while (!mb.try_get(out) || out.empty()) {
+        rl::yield(1, $);
+      }
+      RL_ASSERT(out[0] == 1);
+      RL_ASSERT(shared_data($) == 42);
+    }
+  }
+};
+
+struct mailbox_multi : rl::test_suite<mailbox_multi, 2> {
+  static constexpr int kCount = 3;
+  tools::mailbox<move_only> mb;
+
+  void thread(unsigned idx) {
+    if (idx == 0) {
+      for (int i = 1; i <= kCount; ++i) {
+        mb.push(move_only{i});
+      }
+    } else {
+      std::vector<move_only> collected;
+      while (static_cast<int>(collected.size()) < kCount) {
+        std::vector<move_only> batch;
+        mb.try_get(batch);
+        for (auto& item : batch) {
+          RL_ASSERT(!item.moved_from_);
+          collected.push_back(std::move(item));
+        }
+        if (batch.empty()) {
+          rl::yield(1, $);
+        }
+      }
+      RL_ASSERT(static_cast<int>(collected.size()) == kCount);
+      for (int i = 0; i < kCount; ++i) {
+        RL_ASSERT(!collected[i].moved_from_);
+        RL_ASSERT(collected[i].val_ == i + 1);
+      }
+    }
+  }
+};
+
+int main() {
+  rl::simulate<mailbox_simple>();
+  rl::simulate<mailbox_memory_order>();
+  rl::simulate<mailbox_multi>();
+}
