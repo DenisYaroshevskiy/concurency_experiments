49d34dfc7072d7e20ab31b3db038a9cf49a64d2e
dyaroshev <denis.yaroshevskij@gmail.com>
2026-04-05 22:02:30 +0100

    use blocking wait for rcu_2

--- include/owner_stealer.h
+++ include/owner_stealer.h
@@ -1,7 +1,6 @@
 // Copyright 2026 Denis Yaroshevskiy
 // Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE.md or copy at
-// https://www.boost.org/LICENSE_1_0.txt)
+// (See accompanying file LICENSE.md or copy at https://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
 
